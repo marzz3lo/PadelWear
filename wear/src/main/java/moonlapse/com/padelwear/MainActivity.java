a884d6d4a729da3f45634e989e5ebeec96edc102
marzz3lo <marzzelo@gmail.com>
2017-06-24 00:11:16 -0400

    Ejercicio: Creación de Layouts Curvados

--- wear/src/main/java/moonlapse/com/padelwear/MainActivity.java
+++ wear/src/main/java/moonlapse/com/padelwear/MainActivity.java
@@ -3,6 +3,7 @@ package moonlapse.com.padelwear;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.wearable.view.CurvedChildLayoutManager;
 import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.view.WearableRecyclerView;
 import android.view.View;
@@ -32,5 +33,8 @@ public class MainActivity extends Activity {
 
         });
         lista.setAdapter(adaptador);
+        lista.setCenterEdgeItems(true);
+        lista.setLayoutManager(new CurvedChildLayoutManager(this));
+
     }
 }
