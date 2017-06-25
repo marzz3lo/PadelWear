b72d96344cc3d7073417343c8f02b0194f04e6b1
marzz3lo <marzzelo@gmail.com>
2017-06-24 22:47:53 -0400

    Ejercicio: Habilitar modo ambiente en la actividad Contador

--- wear/src/main/java/moonlapse/com/padelwear/Contador.java
+++ wear/src/main/java/moonlapse/com/padelwear/Contador.java
@@ -2,13 +2,18 @@ package moonlapse.com.padelwear;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.DismissOverlayView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import moonlapse.com.comun.DireccionesGestureDetector;
 import moonlapse.com.comun.Partida;
@@ -17,7 +22,7 @@ import moonlapse.com.comun.Partida;
  * Created by marzzelo on 24/6/2017.
  */
 
-public class Contador extends Activity {
+public class Contador extends WearableActivity {
     private Partida partida;
     private TextView misPuntos, misJuegos, misSets, susPuntos, susJuegos, susSets;
     private Vibrator vibrador;
@@ -25,10 +30,18 @@ public class Contador extends Activity {
     private long[] vibrDeshacer = {0l, 500, 500, 500};
     private DismissOverlayView dismissOverlay;
 
+    private Typeface fuenteNormal = Typeface.create("sans-serif", 0);
+    private Typeface fuenteFina = Typeface.create("sans-serif-thin", 0);
+    private TextView hora;
+    private Calendar c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contador);
+        setAmbientEnabled();
+        c = Calendar.getInstance();
+        c.setTime(new Date());
 
         dismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
         dismissOverlay.setIntroText("Para salir de la aplicación, haz una pulsación larga");
@@ -42,6 +55,11 @@ public class Contador extends Activity {
         susJuegos = (TextView) findViewById(R.id.susJuegos);
         misSets = (TextView) findViewById(R.id.misSets);
         susSets = (TextView) findViewById(R.id.susSets);
+        hora = (TextView) findViewById(R.id.hora);
+
+        hora.setText(c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE));
+
+
         actualizaNumeros();
         View fondo = findViewById(R.id.fondo);
         fondo.setOnTouchListener(new View.OnTouchListener() {
@@ -120,6 +138,42 @@ public class Contador extends Activity {
         });
     }
 
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        misPuntos.setTypeface(fuenteFina);
+        misPuntos.getPaint().setAntiAlias(false);
+        susPuntos.setTypeface(fuenteFina);
+        susPuntos.getPaint().setAntiAlias(false);
+        misJuegos.setTypeface(fuenteFina);
+        misJuegos.getPaint().setAntiAlias(false);
+        susJuegos.setTypeface(fuenteFina);
+        susJuegos.getPaint().setAntiAlias(false);
+        misSets.setTypeface(fuenteFina);
+        misSets.getPaint().setAntiAlias(false);
+        susSets.setTypeface(fuenteFina);
+        susSets.getPaint().setAntiAlias(false);
+        hora.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onExitAmbient() {
+        super.onExitAmbient();
+        misPuntos.setTypeface(fuenteNormal);
+        misPuntos.getPaint().setAntiAlias(true);
+        susPuntos.setTypeface(fuenteNormal);
+        susPuntos.getPaint().setAntiAlias(true);
+        misJuegos.setTypeface(fuenteNormal);
+        misJuegos.getPaint().setAntiAlias(true);
+        susJuegos.setTypeface(fuenteNormal);
+        susJuegos.getPaint().setAntiAlias(true);
+        misSets.setTypeface(fuenteNormal);
+        misSets.getPaint().setAntiAlias(true);
+        susSets.setTypeface(fuenteNormal);
+        susSets.getPaint().setAntiAlias(true);
+        hora.setVisibility(View.GONE);
+    }
+
     void actualizaNumeros() {
         misPuntos.setText(partida.getMisPuntos());
         susPuntos.setText(partida.getSusPuntos());
@@ -127,5 +181,11 @@ public class Contador extends Activity {
         susJuegos.setText(partida.getSusJuegos());
         misSets.setText(partida.getMisSets());
         susSets.setText(partida.getSusSets());
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+        hora.setText(c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE));
     }
 }
